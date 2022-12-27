a8b0a6f102e0ed4974fa752fb849cb5f16f271bb
patipan-patisampita <patipan.patisampita@gmail.com>
2022-12-27 10:27:39 +0700

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'gridview_page.dart';
 import 'home.dart';
 
 class MyApp extends StatelessWidget{
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home: HomePage(),
+      home: GridViewPage(),
     );
   }
 }
